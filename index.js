b93db7f22d7554bd742b239b84a47fb3a3c88fdb
Nicolas Telep <ndtelep@gmail.com>
2022-08-09 15:46:46 -0600

    commit tree e454b42b7d08c4f08d7a4f1f5309ff7afd922253

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your solution in this file!
+var customerName = "bob";
+function upperCaseCustomerName (){
+    customerName = "BOB";
+}
+function setBestCustomer (){
+    bestCustomer = "not bob";
+}
+function overwriteBestCustomer (){
+    bestCustomer = "maybe bob"
+}
+const leastFavoriteCustomer = "Johnny"
+function changeLeastFavoriteCustomer (){
+    leastFavoriteCustomer = "Paulo"
+}
